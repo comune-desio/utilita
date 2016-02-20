f986b7478037fa4a3fae302e9e29e5c15713d627
Maurizio De Magnis <root@olisti.co>
2016-02-20 10:25:31 +0100

    Aggiunge uno script di conversione
    Lo script converte un file `.csv` nel formato `.geojson`.

---
+++ csv2geojson.rb
@@ -0,0 +1,33 @@
+require "csv"
+require "json"
+
+path = ARGV[0]
+
+features = CSV.foreach(path, col_sep: ",", headers: true).map do |row|
+  title = "#{row["name"]}, #{row["TOPONIMO"]} #{row["INDIRIZZO"]} #{row["CIVICO"]}"
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [
+        row["longitude"],
+        row["latitude"],
+      ]
+    },
+    properties: {
+      title: title,
+      description: row["DESCRIZIONE"],
+      "marker-symbol": "heart",
+      "marker-color": "#E56C69",
+    }
+  }
+end
+
+document = {
+  type: "FeatureCollection",
+  features: features
+}
+
+dest_filename = "#{File.basename(path, ".csv")}.geojson"
+
+File.write(dest_filename, document.to_json)
